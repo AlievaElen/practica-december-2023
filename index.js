865ecdedc7604cb110ed40779e249f3bd62b1db2
= <=>
2024-02-05 15:22:52 +0300

    commit

--- index.js
+++ index.js
@@ -4,16 +4,38 @@
 //     });
 // });
 
-// $(document).ready(function(){
-//     $(".header__button").click(function(){
-//         $("button").hide(1000);
-//     });
-//     $("button").click(function(){
-//         $(this).css("background-color", "#ffffff");
-//     });
-// });
-$("button").click(function(){
-    $(this).toggle(1000, function(){
-        $(this).css("background-color","#363A4D")
-    })
+$(document).ready(function(){
+    $(".header__button").click(function(){
+        $("button").hide(1000);
+    });
+    $("button").click(function(){
+        $(this).css("background-color", "#ffffff");
+    });
+});
+
+// $("button").click(function(){
+//     $(this).toggle(1000, function(){
+//         $(this).css("background-color","#363A4D")
+//     })
+// })
+
+$(document).ready(function(){
+$("img").on({
+    mouseenter:function(){
+        $(this).css("width", "+=5px")
+        $(this).css( "height", "+=5px")
+        $(this).css(  "opacity", "0.8")
+    }, 
+    mouseleave:function(){
+        $(this).css("width", "-=5px")
+        $(this).css( "height", "-=5px")
+        $(this).css(  "opacity", "1")
+    }
+})
+})
+$(document).ready(function(){
+$(".main__container").hover(function(){
+    $(this).css("background-color", "#bbb")
+})
+
 })
